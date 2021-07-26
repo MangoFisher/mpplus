0bfb3b566ac5ef75c4b0d36d94f0a4389cb6b70a
mangofish <576027405@qq.com>
2021-07-26 21:12:50 +0800

    mybatisplus 实现自动填充

--- src/main/java/com/mango/mpplus/entity/User.java
+++ src/main/java/com/mango/mpplus/entity/User.java
@@ -1,9 +1,12 @@
 package com.mango.mpplus.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
@@ -12,4 +15,9 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+    //实体类中驼峰命名的变量，同数据中带下划线的列一一对应（即create_time）
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
 }
---
+++ src/main/java/com/mango/mpplus/handler/MyMetaObjectHandler.java
@@ -0,0 +1,23 @@
+package com.mango.mpplus.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+//实现某个类的属性的自动填充
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
