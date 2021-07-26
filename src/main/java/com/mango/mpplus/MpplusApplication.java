98d63f72c78bdc40c8acb107d8b2c6fe77f229fe
mangofish <576027405@qq.com>
2021-07-26 14:53:53 +0800

    初始化项目

---
+++ src/main/java/com/mango/mpplus/MpplusApplication.java
@@ -0,0 +1,13 @@
+package com.mango.mpplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpplusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mango/mpplus/MpplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.mpplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
