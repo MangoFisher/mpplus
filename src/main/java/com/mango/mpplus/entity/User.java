5b0d58bb2bd5b32714ed0e7072721c5c47272de2
mangofish <576027405@qq.com>
2021-07-26 20:56:25 +0800

    实现id自增

--- src/main/java/com/mango/mpplus/entity/User.java
+++ src/main/java/com/mango/mpplus/entity/User.java
@@ -1,9 +1,13 @@
 package com.mango.mpplus.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class User {
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String name;
     private Integer age;
